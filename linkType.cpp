6edd201aa94b0200f0c29d33b11befef9dbae2bc
Rich Gautier <rgautier@gmail.com>
2016-04-03 14:30:27 -0400

    class linkType will be used to define associations for memories

---
+++ linkType.cpp
@@ -0,0 +1,12 @@
+
+#include <string>
+#include "cLinkType.h"
+
+cLinkType:cLinkType(int str, string inverb) {
+	strength = str;
+	verb = verb
+}
+
+cLinkType:cLinkType() {
+	strength = 10;
+}
---
+++ linkType.h
@@ -0,0 +1,20 @@
+// linkType.h
+#ifndef LINKTYPE_H
+#define LINKTYPE_H
+
+#include <string>
+
+class cLinkType {
+// A link type talks about the properties of how something is associated
+// The default linktype is JUST that they're associated somehow - doesn't say how.
+// Should also have a strength of association.
+// Methods may cement or weaken the relationship or remove it altogether
+
+	int strength;
+	string verb = "is associated with";
+	public:
+	cLinkType(int str,string inverb);
+        cLinkType();
+};
+
+#endif
