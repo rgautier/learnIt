a01e0876ee20fd274c3384043f3105c16885ed1c
Rich Gautier <rgautier@gmail.com>
2016-04-03 12:49:55 -0400

    Changing to C++ because I probably should push myself to learn more

---
+++ learnIt.cpp
@@ -0,0 +1,20 @@
+/* 
+   LearnIt - A C program to:
+ 	Read shit off the web.
+        Do Natural Language Processing to understand written structure.
+        Build a neural map of relationships between nouns/objects and hopefully learn new connected relations.
+	Have a reputation system for determining 'truthfulness' of facts.
+	Allow for a 'parental' figure to ensure morals via fact-check. (95-100% reputation factor)
+        Store information in database.
+	Load database on startup - go into learn mode
+	Have a query engine/processor.
+	Have a personality/info sharing 'posting' engine that attempts to share strongly held beliefs.
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	cout << "test" << endl;
+	return 0;
+}
